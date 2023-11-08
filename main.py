3c8caf21ac6c2efd961a2d1bc6499334b6242f4d
saverio_medici <134234321+Clu46@users.noreply.github.com>
2023-11-08 20:28:44 +0100

    Update main.py
    Correggendo errori, migliorando il codice

--- main.py
+++ main.py
@@ -2,28 +2,13 @@ import turtle as obj
 from time import sleep, perf_counter
 from math import sin, cos, radians
 
-"""
-
-Data la velocità iniziale in metri al secondo, simulare la traiettoria di un proiettile sparato con la velocità iniziale presa in input
-
-delta_t = 0.01 secondi
-
-#
-#       x = x0 + v0 * cos(a) * delta_t
-#       y = -1/2 * g * (delta_t ^ 2) + v0 * sin(a) + y0
-#
-
-
-"""
-
-obj.title("Simulazione sparo proiettile")
-
 g = 9.81
 
 def set_env():
     terrain = obj
 
-    terrain.title("Simulazione proiettile")
+    
+    terrain.title("Parabolic Motion Simulator")
     terrain.hideturtle()
     terrain.penup()
     
@@ -32,20 +17,20 @@ def set_env():
     terrain.pendown()
     terrain.setpos(500, -200)
 
-def set_proiettile(y):
+def set_proiettile():
     obj.hideturtle()
     obj.penup()
 
     obj.goto(-500, 0)
     
-    obj.color('black')
+    obj.color("black")
     obj.shape("circle")
 
     obj.shapesize(0.3, 0.3, 0.3)
-    
+
     obj.showturtle()
     obj.pendown()
-
+        
 def take_input():
     v = obj.numinput("Velocità iniziale", "Inserire la velocità con cui il proiettile viene sparato: ", 10, 1, 100)
     # h = obj.numinput("Altezza iniziale", "Inserire l'altezza da cui proiettile viene sparato: ", 10, 1, 100)
@@ -53,12 +38,7 @@ def take_input():
     
     return v, teta
 
-
-
 def aggiorna_xyv(vx, vy, teta, delta_t):
-
-
-
     x = obj.xcor() + vx*delta_t
 
     y = (-1/2) * g * (delta_t ** 2) + vy*delta_t + obj.ycor()
@@ -69,13 +49,17 @@ def aggiorna_xyv(vx, vy, teta, delta_t):
 
 
 def move_proiettile(v0, teta):
+    set_proiettile()
+
     vx = v0*cos(teta)
     vy = v0*sin(teta)
 
 
-    while obj.ycor() >= 0:
+    while obj.ycor() >= -200:
         x, y, vy = aggiorna_xyv(vx, vy, teta, 0.01)
-
+        
+        obj.write("Home = ", True, align="center")
+        
         obj.goto(x, y)
 
 if __name__ == "__main__":
@@ -85,4 +69,4 @@ if __name__ == "__main__":
 
     
     move_proiettile(velocita, radians(angolo))
-    obj.done()
+    obj.mainloop()
